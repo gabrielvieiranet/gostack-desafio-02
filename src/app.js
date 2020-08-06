6401a3d2f80edeb1394da4554e23964994f6eb9b
gabriel <forrest10ster@gmail.com>
2020-08-06 15:03:10 -0300

    preenchimento de métodos REST

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { v4 } = require("uuid");
 
 const app = express();
 
@@ -11,23 +10,71 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repo = {
+    id: v4(),
+    title,
+    url,
+    techs,
+    likes: 0
+  };
+
+  repositories.push(repo);
+
+  return response.json(repo);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repoIndex = repositories.findIndex(repo => repo.id === id);
+  if(repoIndex < 0) {
+    return response.status(400).json({ error: 'Invalid repository' });
+  }
+
+  const repo = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repoIndex].likes,
+  }
+
+  repositories[repoIndex] = repo;
+
+  return response.json(repo);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repoIndex = repositories.findIndex(repo => repo.id === id);
+
+  if(repoIndex < 0) {
+    return response.status(400).json({ error: 'Invalid repository' });
+  }
+
+  repositories.splice(repoIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repoIndex = repositories.findIndex(repo => repo.id === id);
+
+  if(repoIndex < 0) {
+    return response.status(400).json({ error: 'Invalid repository' });
+  }
+
+  repositories[repoIndex].likes++;
+
+  return response.json(repositories[repoIndex]);
 });
 
 module.exports = app;
